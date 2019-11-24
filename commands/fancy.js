5388a8d756c2265a8b176585f07aa38700d08baa
SaintMorning <marcostoecker95@gmail.com>
2019-11-24 07:20:24 +0100

    fancy changes

--- commands/fancy.js
+++ commands/fancy.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const translator = require('custom-translate')
 const fancyDictionary = require('../assets/json/fancy')
+const moment = require('moment')
 
 module.exports = {
 	name: 'fancy',
@@ -12,10 +13,21 @@ module.exports = {
 	cooldown: 5,
 	//listed: 0,
 	execute(message, arguments) {
+		const date = moment()
 		if (message.content.length > 2000) return message.channel.send('Maximum text size exceeded (2000)');
 
 		fancied_message = translator.letterTrans(arguments.join(' '), fancyDictionary);
-		message.channel.send(fancied_message);
+		message.channel.send(`${fancied_message}\n - ${message.author} ${date.format('dddd, MMMM Do YYYY, HH:mm:ss')}`);
 
+		//const embed = new Discord.MessageEmbed()
+		// .setAuthor(`@${message.author.tag}`)
+		// .setThumbnail('https://i.imgur.com/wSTFkRM.png')
+		// .addField(`Nachricht von ${message.member.user}`, fancied_message)
+		// .addBlankField()
+		// .addField('Nachricht', message.member.user)
+		// .setColor('RANDOM')
+
+		//message.channel.send(embed);
+		
 	},
 };
